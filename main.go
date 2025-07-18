010a105c8b309499d9c9012188db7d1e7b663c74
MarshPeter <peter.m.farmer.204@gmail.com>
2025-07-18 17:39:48 +1000

    file reading is working

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
+	f, err := os.Open("./test_file.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	br := bufio.NewReader(f)
+
+	for {
+		b, err := br.ReadByte()
+
+		if err != nil && !errors.Is(err, io.EOF) {
+			panic(err)
+		}
+
+		fmt.Printf("%c", b)
+
+		if err != nil {
+			break
+		}
+	}
+
 	fmt.Println("Hello, World")
 }
