b2f018efb2b5d8a25f94c401840618f46d47e08c
MarshPeter <peter.m.farmer.204@gmail.com>
2025-07-19 21:41:56 +1000

    everything standard input from cat is working, therefore piped text as a whole should work

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 	"slices"
 )
 
-type FileStats struct {
+const (
+	BYTE_FLAG      string = "-c"
+	LINE_FLAG      string = "-l"
+	WORD_FLAG      string = "-w"
+	CHARACTER_FLAG string = "-m"
+	ALL_FLAG       string = "-c-l-w"
+)
+
+type TextStats struct {
 	ByteCount      int
 	LineCount      int
 	WordCount      int
@@ -26,8 +34,8 @@ func GetFile(fileName string) *os.File {
 	return f
 }
 
-func GetContentStatistics(br *bufio.Reader) FileStats {
-	fileInformation := FileStats{}
+func GetContentStatistics(br *bufio.Reader) TextStats {
+	fileInformation := TextStats{}
 
 	whitespace_characters := []rune{0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x20}
 
@@ -66,38 +74,76 @@ func GetContentStatistics(br *bufio.Reader) FileStats {
 	return fileInformation
 }
 
-func EmitResult(code string, info FileStats, fileName string) {
+func EmitResult(code string, info TextStats, fileName string) {
 
 	switch code {
-	case "-c":
+	case BYTE_FLAG:
 		fmt.Printf("%d %s\n", info.ByteCount, fileName)
-	case "-l":
+	case LINE_FLAG:
 		fmt.Printf("%d %s\n", info.LineCount, fileName)
-	case "-w":
+	case WORD_FLAG:
 		fmt.Printf("%d %s\n", info.WordCount, fileName)
-	case "-m":
+	case CHARACTER_FLAG:
 		fmt.Printf("%d %s\n", info.CharacterCount, fileName)
+	case ALL_FLAG:
+		fmt.Printf("  %d %d %d %s\n", info.LineCount, info.WordCount, info.ByteCount, fileName)
 	}
+}
 
+func IsFlag(str string) bool {
+	return str == BYTE_FLAG ||
+		str == LINE_FLAG ||
+		str == WORD_FLAG ||
+		str == CHARACTER_FLAG ||
+		str == ALL_FLAG
 }
 
 func main() {
-
 	args := os.Args[1:]
 
+	var fileName string
+	var code string
+	isFileBased := true
+
 	if len(args) == 0 {
-		panic("You must include a file or flags to read from")
+		code = ALL_FLAG
+		isFileBased = false
 	}
 
-	fileName := args[1]
+	if len(args) == 1 {
+		if IsFlag(args[0]) {
+			code = args[0]
+			isFileBased = false
+		} else {
+			fileName = args[0]
+		}
+	}
 
-	f := GetFile(fileName)
+	if len(args) == 2 {
+		code = args[0]
+		fileName = args[1]
+	}
 
-	br := bufio.NewReader(f)
+	var br *bufio.Reader
 
-	fileInformation := GetContentStatistics(br)
+	if isFileBased {
+		f := GetFile(fileName)
+		br = bufio.NewReader(f)
+	} else {
+		br = bufio.NewReader(os.Stdin)
+	}
 
-	code := args[0]
+	textInformation := GetContentStatistics(br)
 
-	EmitResult(code, fileInformation, fileName)
+	if len(args) == 2 {
+		EmitResult(code, textInformation, fileName)
+	}
+
+	if len(args) == 1 && isFileBased {
+		EmitResult("-c-l-w", textInformation, fileName)
+	}
+
+	if len(args) < 2 && !isFileBased {
+		EmitResult(code, textInformation, "")
+	}
 }
